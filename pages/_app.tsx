6ff506d1876f813ba0f3d32585b2b366857fe7f3
kiner-tang(文辉) <1127031143@qq.com>
2023-04-01 10:39:57 +0800

    feat: support inject style into html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../public/antd.min.css';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import withTheme from '../theme';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import withTheme from "../theme";
+// 1.1 inject style into html to optimize the first screen rendering experience
+import antdStyle from "../theme/output/antdStyle";
+// 2. import extra css file in js
+// import "../theme/output/antd.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return withTheme(<Component {...pageProps} />);
+  return (
+    <>
+      {/* 1.2 inject style into html */}
+      <style jsx global>{`
+        ${antdStyle}
+      `}</style>
+      {withTheme(<Component {...pageProps} />)}
+    </>
+  );
 }
--- scripts/genAntdCss.tsx
+++ scripts/genAntdCss.tsx
@@ -1,8 +1,14 @@
 import fs from "fs";
+import path from "path";
 import { extractStyle } from "@ant-design/static-style-extract";
 import withTheme from "../theme";
 
-const outputPath = "./public/antd.min.css";
+const outputStylePath = "./theme/output/antdStyle.ts";
+const outputCssPath = "./theme/output/antd.min.css";
+
+if (!fs.existsSync(path.dirname(outputStylePath))) {
+    fs.mkdirSync(path.dirname(outputStylePath), { recursive: true });
+}
 
 // 1. default theme
 
@@ -12,6 +18,7 @@ const outputPath = "./public/antd.min.css";
 
 const css = extractStyle(withTheme);
 
-fs.writeFileSync(outputPath, css);
+fs.writeFileSync(outputStylePath, `export default \`${css.replace(/\\/g, '\\\\')}\``);
+fs.writeFileSync(outputCssPath, css);
 
-console.log(`🎉 Antd CSS generated at ${outputPath}`);
+console.log(`🎉 Antd CSS generated at \n- ${outputStylePath}: if you want to inject style into html to optimize the first screen rendering experience.\n- ${outputCssPath}: If you want to import extra css file in js.`);
