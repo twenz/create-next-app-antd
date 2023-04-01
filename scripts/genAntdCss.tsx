b8f1577e0ae49b610a625eceb6320ef9446b17e0
kiner-tang(文辉) <1127031143@qq.com>
2023-04-01 10:55:25 +0800

    feat: optimize code

--- scripts/genAntdCss.tsx
+++ scripts/genAntdCss.tsx
@@ -4,9 +4,13 @@ import { extractStyle } from "@ant-design/static-style-extract";
 import withTheme from "../theme";
 
 const outputDir = "./theme/output";
+const outputStyleFileName = "antdStyle.ts";
+const outputCssFileName = "antd.min.css";
 
-const outputStylePath = path.resolve(outputDir, "antdStyle.ts");
-const outputCssPath = path.resolve(outputDir, "antd.min.css");
+const outputStylePath = path.resolve(outputDir, outputStyleFileName);
+const outputCssPath = path.resolve(outputDir, outputCssFileName);
+
+const displayPath = (name: string) => `${outputDir}/${name}`;
 
 if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
@@ -23,4 +27,4 @@ const css = extractStyle(withTheme);
 fs.writeFileSync(outputStylePath, `export default \`${css.replace(/\\/g, '\\\\')}\``);
 fs.writeFileSync(outputCssPath, css);
 
-console.log(`🎉 Antd CSS generated at \n- ${outputStylePath}: if you want to inject style into html to optimize the first screen rendering experience.\n- ${outputCssPath}: If you want to import extra css file in js.`);
+console.log(`🎉 Antd CSS generated at \n- ${displayPath(outputStyleFileName)}: if you want to inject style into html to optimize the first screen rendering experience.\n- ${displayPath(outputCssFileName)}: If you want to import extra css file in js.`);
