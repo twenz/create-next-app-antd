e69b2d477d9e264c4243f39ef159bbee5b625370
kiner-tang(文辉) <1127031143@qq.com>
2023-04-02 15:00:13 +0800

    feat: add demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import withTheme from "../theme";
-// 1.1 inject style into html to optimize the first screen rendering experience
-import antdStyle from "../theme/output/antdStyle";
+import { extractStyle } from "@ant-design/static-style-extract";
 // 2. import extra css file in js
 // import "../theme/output/antd.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  // 1.1 inject style into html to optimize the first screen rendering experience
+  const antdStyle = extractStyle(() => withTheme(<Component {...pageProps} />));
+  // PS: extra the full style
+  // const antdStyle = extractStyle((node) => withTheme(<><Component {...pageProps} />{node}</>));
+
   return (
     <>
       {/* 1.2 inject style into html */}
---
+++ pages/detail.tsx
@@ -0,0 +1,18 @@
+import { Form, InputNumber, Switch } from "antd";
+
+export default function Home() {
+  return (
+    <>
+      <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+        <Form.Item label="数字输入框">
+          <InputNumber min={1} max={10} defaultValue={3} />
+          <span className="ant-form-text"> 台机器</span>
+          <a href="https://ant.design">链接文字</a>
+        </Form.Item>
+        <Form.Item label="开关">
+          <Switch defaultChecked />
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
