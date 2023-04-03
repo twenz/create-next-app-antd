fd9d7466cbb418468c1da99d5ba87b864882736d
kiner-tang(文辉) <1127031143@qq.com>
2023-04-03 15:46:42 +0800

    feat: optimize code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import withTheme from "../theme";
-import { extractStyle } from "@ant-design/static-style-extract";
-
-// 2.1 add following line into package.json's scripts
-// "predev": "ts-node --project ./tsconfig.node.json ./scripts/genAntdCss.tsx",
-// "prebuild": "ts-node --project ./tsconfig.node.json ./scripts/genAntdCss.tsx"
-// 2.2 import extra css file in js
-// import "../theme/output/antd.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // 1.1 inject style into html to optimize the first screen rendering experience
-  // const antdStyle = extractStyle(() => withTheme(<Component {...pageProps} />));
-  // PS: extra the full style
-  // const antdStyle = extractStyle((node) => withTheme(<><Component {...pageProps} />{node}</>));
-
   return (
     <>
-      {/* 1.2 inject style into html */}
-      {/* <style jsx global>{`
-        ${antdStyle}
-      `}</style> */}
       {withTheme(<Component {...pageProps} />)}
     </>
   );
