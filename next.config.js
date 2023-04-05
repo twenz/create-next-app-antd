337cbb28ca2deca04ed44a6d7f009448f18ce063
kiner-tang(文辉) <1127031143@qq.com>
2023-04-05 12:08:30 +0800

    feat: output path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const distDir = process.env.NODE_ENV === 'production' ? '_next' : '.next';
+
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd'],
+  distDir,
 };
 
 module.exports = nextConfig;
